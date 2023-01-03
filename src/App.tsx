dfd89066db8a9848e47c928b965ea7338d188448
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2023-01-03 23:54:26 +0900

    feat : pagination react 버전으로 수정중

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,14 @@ const App = () => {
   const [_data, setData] = useState<PokeName[]>([]);
   const [loading, setLoading] = useState(false);
   const [offset, setOffset] = useState<number>(0); // start, end = 20 고정
-  const [totalPage, setTotalPage] = useState<number>(0);
+  // data 크기
+  const [_dataLength, setDataLength] = useState<number>(0);
+
+  const [page, setPage] = useState<number>(1);
+
+  // 한 페이지당 보여줄 data 갯수
+  const listPerPage = 20;
+  const totalPage = Math.ceil(_dataLength / listPerPage);
 
   const URL = "https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0";
 
@@ -20,33 +27,21 @@ const App = () => {
         throw new Error(`HTTP Error : status code is ${_res.status}`);
       const _json = await _res.json();
       const _object = _json.results as PokeName[];
+      setDataLength(_object.length);
       setData(_object);
       setLoading(false);
     });
   };
 
-  /** 총 페이지수 : Math.ceil(모든 전체 갯수 / 보여줄 갯수)
-  화면에 보여질 페이지 한 줄 :  Math.ceil(현재 페이지 / 한 줄에 보여지는 페이지 수)
-  화면에 그릴 첫 페이지 : 화면에 그릴 막 페이지 - ( 화면에 보여질 페이지 한 줄 - 1)
-  화면에 그릴 막 페이지 : 현재 페이지 그룹 * 화면에 보여질 페이지 한 줄
-  */
-
-  // data 갯수
-  const _totalCount = Math.ceil(_data.length / 20); // 20개를 한 화면에 보여줌
-
-  if (_totalCount <= 20) return;
-
-  setTotalPage(Math.ceil(_totalCount / 20)); //총 페이지수
-
   useEffect(() => {
     fetchPokemon();
+    console.log(_dataLength)
   }, []);
 
   if (loading) return <strong>Loading...</strong>;
   return (
     <>
       <Pokedata info={_data} />
-      <Pagination start={1} end={5} />
     </>
   );
 };
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Pagination({ start, end }) {
-  /** 총 페이지수 Math.ceil(모든 전체 갯수 / 보여줄 갯수)
-  화면에 보여질 페이지 한 줄 Math.ceil(현재 페이지 / 한 줄에 보여지는 페이지 수)
+interface PaginationType {
+  totalPage : number;
+  limit : number;
+  page : number;
+  setPage : number;
+}
+
+export default function Pagination({ totalPage, limit, page, setPage }:PaginationType) {
+  // 총 페이지 갯수에 따라 Pagination 갯수 정하기, limit 단위로 페이지 리스트 넘기기
+  
+  /** 총 페이지수 : Math.ceil(모든 전체 갯수 / 보여줄 갯수)
+  화면에 보여질 페이지 한 줄 :  Math.ceil(현재 페이지 / 한 줄에 보여지는 페이지 수)
+  화면에 그릴 첫 페이지 : 화면에 그릴 막 페이지 - ( 화면에 보여질 페이지 한 줄 - 1)
+  화면에 그릴 막 페이지 : 현재 페이지 그룹 * 화면에 보여질 페이지 한 줄
   */
-  console.log(start, end);
+  const [currentPageArray, setCurrentPageArray] = useState([]);
+  const [totalPageArray, setToatalPageArray] = useState([]);
+
+  useEffect(() => {
+    if (page % limit === 1) {
+      setCurrentPageArray(totalPageArray[Math.floor(page / limit)])
+    } else if (page % limit === 0) {
+      setCurrentPageArray(totalPageArray)
+    }
+  })
+  
   return <div></div>;
 }
